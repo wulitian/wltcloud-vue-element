4add4d6adad7c9cafffdbae33d27d42a20ac386d
13344 <1334416530@qq.com>
2019-08-03 10:23:14 +0800

    修改配置

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import store from './vuex/store.js'
 import '../theme/index.css'
+import '../static/css/wulitian-reset.scss'
 import { Button, Select } from 'element-ui'
 Vue.use(Button)
 Vue.use(Select)
